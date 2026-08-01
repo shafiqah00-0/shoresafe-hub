c03455c1b4ddc11dc81a42e270e0c097f8c4868d
shafiqah00-0 <iqashafiqaho9@gmail.com>
2026-08-02 02:45:27 +0800

    Update code

--- config/database.php
+++ config/database.php
@@ -14,7 +14,7 @@ try {
 
     $pdo = new PDO($dsn, $user, $password, [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO_FETCH_ASSOC,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES   => false,
     ]);
 } catch (PDOException $e) {
